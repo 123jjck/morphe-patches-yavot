296e137e63f79493fedea773c1d415cc1da94064
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-05-06 15:11:48 +0200

    fix: Add YouTube patch exception message for users who ignore the Manager update prompt

--- patches/src/main/kotlin/app/morphe/patches/all/misc/updates/CheckPatcherUpToDatePatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/all/misc/updates/CheckPatcherUpToDatePatch.kt
@@ -3,7 +3,6 @@ package app.morphe.patches.all.misc.updates
 import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.methodCall
 import app.morphe.patcher.patch.bytecodePatch
-import app.morphe.patcher.patch.resourcePatch
 
 // TODO: Delete this after Manager 1.17.0 has been released
 internal val checkPatcherUpToDatePatch = bytecodePatch {
@@ -11,7 +10,7 @@ internal val checkPatcherUpToDatePatch = bytecodePatch {
         try {
             Fingerprint(
                 filters = listOf(
-                    methodCall()
+                    methodCall(definingClass = "Lapp/morphe/extension/shared/Utils;")
                 )
             ).instructionMatches.first().getMethodCalled()
         } catch (_: Throwable) {
